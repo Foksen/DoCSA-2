f96ea5abf2ffdc52a1489b60f238d41351c635fc
Foksen <izholobov2004@gmail.com>
2025-09-17 22:37:55 +0300

    Pr2 - task1

---
+++ Pr2/src/main/java/ru/mirea/task1/NioReader.java
@@ -0,0 +1,23 @@
+package ru.mirea.task1;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class NioReader {
+
+    private static final String FILENAME = "tmp/example.txt";
+
+    public static void main(String[] args) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(FILENAME), StandardCharsets.UTF_8);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+    }
+}
